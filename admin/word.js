485ad1772c93e399f74ca42a1a6db3c521485b36
Zefau <Zefau@users.noreply.github.com>
2018-10-27 11:06:50 +0200

    Create word.js

---
+++ admin/word.js
@@ -0,0 +1,29 @@
+'use strict';
+
+systemDictionary = {
+	"auth": {
+		"en": "Authentication Credentials",
+		"de": "Authentifizierung"
+	},
+	"auth_info": {
+		"en": "",
+		"de": ""
+	},
+    "main": {
+		"en": "Main Settings",
+		"de": "Einstellungen"
+	},
+    "username": {
+		"en": "Username",
+		"de": "Benutzer"
+	},
+    "password": {
+		"en": "Password",
+		"de": "Passwort"
+	},
+    "ip": {
+		"en": "IP Address",
+		"de": "IP Adresse"
+	},
+	
+};
